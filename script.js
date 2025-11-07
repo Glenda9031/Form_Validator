3cce3e29e4fec24491e493363b8adc43bd52d1cd
Glenda Joyce Reed <100007624+Glenda9031@users.noreply.github.com>
2025-11-07 16:27:18 -0600

    Update script.js

--- script.js
+++ script.js
@@ -10,4 +10,37 @@ function showError(input, message) {
     formControl.className = 'form-control error';
     const small = formControl.querySelector('small');
     small.innerText = message;
+
+    // Show success outline
+    function showSuccess(input) {
+        const formControl = input.parentElement;
+        formControl.className = 'form-control success';
+    }
+
+    // Check email is valid
+    function checkEmail(input) {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (re.test(input.value.trim())) {
+            showSuccess(input);
+        } else {
+            showError(input, 'Email is not valid');
+        }
+    }
 }
+
+// Check required fields
+function checkRequired(inputArr) {
+  let isRequired = false;
+  inputArr.forEach(function(input) {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+      isRequired = true;
+    } else {
+      showSuccess(input);
+    }
+  });
+
+  return isRequired;
+}
+
+    
